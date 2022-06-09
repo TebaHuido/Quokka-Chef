a32b84db33534db5c47311b86d5e5db6c0637bfb
TebaHuido <baase.hui@gmail.com>
2022-06-09 02:59:38 -0400

    lectura desde mysql

--- recetario.php
+++ recetario.php
@@ -2,7 +2,13 @@
 include_once'bd.php';
 $gsent=$pdo->prepare('SELECT * FROM intrucciones WHERE ID_RECETA = 1 ORDER BY orden' );
 $gsent->execute();
-$res = $gsent->fetchALL();
+$intrucciones = $gsent->fetchALL();
+$gsent2=$pdo->prepare('SELECT * FROM ingredientes WHERE ID_RECETA = 1' );
+$gsent2->execute();
+$ingredientes = $gsent2->fetchALL();
+$gsent3=$pdo->prepare('SELECT * FROM receta WHERE ID_RECETA = 1' );
+$gsent3->execute();
+$receta = $gsent3->fetchALL()[0];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,17 +17,25 @@ $res = $gsent->fetchALL();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="assets\dist\css\bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <div class="container">
-        <div class="d-flex bd-highlight">
-            <div class="p-2 flex-grow-1 bd-highlight"id="ingredientes">Lorem, ipsum dolor.</div>
-            <div class="p-2 bd-highlight"id="imagen"></div>
-          </div>
+    <h1><?php echo $receta['Nombre']?></h1>
+    <h3><?php echo "puntaje: ".$receta['Puntaje']. " tipo: ".$receta['Tipo'] ." tiempo: ".$receta['Tiempo']." ".$receta['Med_tiempo']." porciones: ".$receta['Porciones']. " autor: ".$receta['Autor'] ?></h3>
+
+    <h2>Ingredientes:</h2>
+    <div class="input-group-prepend">
+
+    <ul class="list-group list-group-flush">
+    <?php foreach($ingredientes as $dato):?>
+        <li class="list-group-item"><input type="checkbox"><?php echo "  ". $dato['ingredientes'] ?></li>
+    <?php endforeach?>
+    </ul>
+
     </div>
     <h2>Pasos a seguir:</h2>
-    <?php foreach($res as $dato):?>
-        <h3><?php echo $dato['orden']," ", $dato['intruccion'] ?></h3>
+    <?php foreach($intrucciones as $dato):?>
+        <h3><?php echo $dato['orden'],". ", $dato['intruccion'] ?></h3>
     <?php endforeach?>
     <script src="ajax.js"></script>
 </body>
