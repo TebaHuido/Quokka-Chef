fd2faeb4c796af3cf76e86b0555aad787da0a158
TebaHuido <baase.hui@gmail.com>
2022-06-16 02:59:30 -0400

    agrega bootstrap

--- recetario.php
+++ recetario.php
@@ -18,11 +18,18 @@ include_once'bd.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="assets\dist\css\bootstrap.min.css" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
+    
+    
+
+    <link rel="stylesheet" href="assets\dist\css\bootstrap.min.css">
+    
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans&family=PT+Sans:wght@400;700&display=swap">
+    <style>
+        h1,h2,h3,h4,h5,h6{
+            font-weight: bold;
+        }
+    </style>
 </head>
 <body>
 
@@ -61,7 +68,7 @@ include_once'bd.php';
     </div>
     <h2>Pasos a seguir:</h2>
     <?php foreach($intrucciones as $dato):?>
-        <h3><?php echo $dato['orden'],". ", $dato['intruccion'] ?></h3>
+        <h3><?php echo $dato['orden'],". ", $dato['intruccion'].'<br><br>' ?></h3>
     <?php endforeach?>
     <script src="ajax.js"></script>
 </body>
