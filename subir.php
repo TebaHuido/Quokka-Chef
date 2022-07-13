0121cda162efd07238334960a4afaad1a01a6f9d
TebaHuido <baase.hui@gmail.com>
2022-07-12 22:41:22 -0400

    mod subir

--- subir.php
+++ subir.php
@@ -1,7 +1,9 @@
 <?php
 include ("bd.php");
 $imagen=addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
-
+$gsent2 = $pdo->prepare('SELECT max(ID_Receta) FROM receta;');
+$gsent2->execute();
+$id = $gsent2->fetchALL()[0];
 $gsent=$pdo->prepare("INSERT INTO `receta`(`Descripcion`, `Nombre`, `ID_Receta`, `Tipo`, `Puntaje`, `Autor`, `Tiempo`, `Med_tiempo`, `Porciones`, `Foto`) VALUES "+
 "('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]','[value-9]','[value-10]')");
 $gsent=$pdo->prepare("INSERT INTO `ingredientes`(`ingredientes`, `ID_Receta`) VALUES ('[value-1]','[value-2]')");
