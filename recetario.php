44da164af0290d211ab9f2f0087a5222f3a631b5
Alex <realfight13@gmail.com>
2022-07-14 23:59:02 -0400

    recetario retoques

--- recetario.php
+++ recetario.php
@@ -21,7 +21,7 @@ $imagenes = $gsent4->fetchALL();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo $receta['Nombre'] ?></title>
 
     <link rel="stylesheet" href="css\bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans&family=PT+Sans:wght@400;700&display=swap">
